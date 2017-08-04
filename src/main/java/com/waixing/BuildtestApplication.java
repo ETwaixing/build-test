bded6967c33d009b92ad9a0540fd4a4c64f0885a
waixing <waixing@yonglang.co>
2017-08-04 15:52:24 +0800

    refs#master 第一次提交

---
+++ src/main/java/com/waixing/BuildtestApplication.java
@@ -0,0 +1,12 @@
+package com.waixing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildtestApplication.class, args);
+	}
+}
---
+++ src/main/java/com/waixing/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.waixing.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试控制器
+ *
+ * Created by yonglang on 2017/8/4.
+ */
+@RestController
+@RequestMapping(value = "/")
+public class TestController {
+
+    /**
+     * 测试控制器
+     */
+    @RequestMapping(value = "test",method = RequestMethod.GET)
+    public String test(){
+        return "Test Success!!!";
+    }
+
+}
